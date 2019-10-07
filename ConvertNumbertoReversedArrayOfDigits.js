3a0154e39cb9e10686ebd7bdbd9d3e0067dcb66c
Gabriela Cepeda <gabcep14@gmail.com>
2019-10-07 18:19:09 -0400

    Add Solution

---
+++ ConvertNumbertoReversedArrayOfDigits.js
@@ -0,0 +1,9 @@
+/*
+Details:
+Given a random number:
+You have to return the digits of this number within an array in reverse order.
+ */
+
+function digitize(n) {
+    return String(n).split('').map(Number).reverse()
+}
